140f5945601924447540399c3dc7bcc6511a4f69
John Burke <johnburke934@gmail.com>
2021-04-11 22:15:27 -0700

    Finish image uploader

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module floofy.dev/ume
 go 1.16
 
 require (
-	github.com/go-chi/chi/v5 v5.0.2 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	go.mongodb.org/mongo-driver v1.5.1 // indirect
+	github.com/go-chi/chi/v5 v5.0.2
+	github.com/sirupsen/logrus v1.8.1
+	go.mongodb.org/mongo-driver v1.5.1
 )
--- routing/images.go
+++ routing/images.go
@@ -4,65 +4,150 @@ import (
 	"context"
 	floof "floofy.dev/ume/mongo"
 	"floofy.dev/ume/util"
+	"fmt"
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
+	"github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"io"
 	"net/http"
+	"strings"
 )
 
-type GoAwayResult struct {
+var types = map[string]string{
+	"png":  "image/png",
+	"jpg":  "image/jpg",
+	"webp": "image/webp",
+	"gif":  "image/gif",
+	"mp4":  "video/mp4",
+}
+
+type Response struct {
 	Message string `json:"message"`
 }
 
-type BucketItemResult struct {}
+type BucketItemResult struct {
+	Filename string `json:"filename"`
+}
+
+func IsValidFileType(t string) bool {
+	_, ok := types[t]
+	return ok
+}
 
 func NewImagesRouter(client *mongo.Client) chi.Router {
 	r := chi.NewRouter()
 	r.Use(middleware.Logger)
 
-	r.Get("/", func (w http.ResponseWriter, r *http.Request) {
-		bucket := floof.RetrieveBucket(client); if bucket == nil {
+	r.Get("/{id}", func(w http.ResponseWriter, r *http.Request) {
+		name := chi.URLParam(r, "id")
+		bucket := floof.RetrieveBucket(client)
+		if bucket == nil {
+			util.WriteJson(w, 500, Response{
+				Message: "Failed to get bucket!",
+			})
 			return
 		}
-
-		cursor, err := bucket.GetFilesCollection().Find(context.TODO(), bson.D{}); if err != nil {
-			print(err.Error())
+		stream, err := bucket.OpenDownloadStreamByName(name)
+		if err != nil {
+			util.WriteJson(w, 404, Response{
+				Message: "Not found!",
+			})
 			return
 		}
-
-		var arr []bson.M
-		if err := cursor.All(context.TODO(), &arr); err != nil {
-			println(err.Error())
-			util.WriteJson(w, r, 200, []BucketItemResult{})
+		s := strings.Split(name, ".")
+		if len(s) < 1 || !IsValidFileType(s[len(s)-1]) {
+			util.WriteJson(w, 400, Response{
+				Message: fmt.Sprintf("Unknown file type %s", s[len(s)-1]),
+			})
+			return
 		}
-
-		util.WriteJson(w, r, 200, arr)
+		_, err = io.Copy(w, stream)
+		if err != nil {
+			util.WriteJson(w, 500, Response{
+				Message: "Failed to copy buffer!",
+			})
+			return
+		}
+		contentType, _ := types[s[len(s)-1]]
+		w.Header().Set("Content-Type", contentType)
+		w.WriteHeader(200)
 	})
 
-	r.Post("/upload", func (w http.ResponseWriter, r *http.Request) {
-		if r.Header.Get("Authorization") == "" {
-			util.WriteJson(w, r, 401, GoAwayResult{
-				Message: "not allowed in this town buckeroo.",
+	r.Get("/", func(w http.ResponseWriter, r *http.Request) {
+		bucket := floof.RetrieveBucket(client)
+		if bucket == nil {
+			util.WriteJson(w, 500, Response{
+				Message: "Failed to get bucket!",
 			})
-		}
-
-		auth := util.Auth()
-		if r.Header.Get("Authorization") != auth {
-			util.WriteJson(w, r, 403, GoAwayResult{
-				Message: "you aren't allowed here buckeroo.",
-			})
-		}
-
-		bucket := floof.RetrieveBucket(client); if bucket == nil {
 			return
 		}
 
-		//data, header, _ := r.FormFile("fdata")
-		//file := util.RandomString()
-		//if _, err := bucket.UploadFromStream(file, ); err != nil {
-		//	return
-		//}
+		cursor, err := bucket.GetFilesCollection().Find(context.TODO(), bson.D{}, options.Find().SetProjection(map[string]int{
+			"filename": 1,
+		}))
+		if err != nil {
+			logrus.Errorf("Failed to lookup images: %s", err.Error())
+			return
+		}
+
+		var arr []BucketItemResult
+		if err := cursor.All(context.TODO(), &arr); err != nil {
+			logrus.Errorf("Failed to decode images: %s", err.Error())
+			util.WriteJson(w, 200, []BucketItemResult{})
+			return
+		}
+
+		if arr == nil {
+			util.WriteJson(w, 200, []BucketItemResult{})
+			return
+		}
+		util.WriteJson(w, 200, arr)
+	})
+
+	r.Post("/upload", func(w http.ResponseWriter, r *http.Request) {
+		if r.Header.Get("Authorization") == "" || r.Header.Get("Authorization") != util.Auth() {
+			util.WriteJson(w, 403, Response{
+				Message: "Unauthorized",
+			})
+			return
+		}
+
+		bucket := floof.RetrieveBucket(client)
+		if bucket == nil {
+			util.WriteJson(w, 500, Response{
+				Message: "Failed to get bucket!",
+			})
+			return
+		}
+
+		data, header, _ := r.FormFile("fdata")
+		if header.Size >= 1073741824 {
+			util.WriteJson(w, 400, Response{
+				Message: "File size exceeds 1GB",
+			})
+			return
+		}
+		s := strings.Split(header.Filename, ".")
+		if len(s) < 1 || !IsValidFileType(s[len(s)-1]) {
+			util.WriteJson(w, 400, Response{
+				Message: fmt.Sprintf("Unknown file type %s", s[len(s)-1]),
+			})
+			return
+		}
+		file := util.RandomString()
+		if _, err := bucket.UploadFromStream(fmt.Sprintf("%s.%s", file, s[len(s)-1]), data); err != nil {
+			logrus.Errorf("Failed to upload file %s (%s): %s", file, header.Filename, err.Error())
+			util.WriteJson(w, 500, Response{
+				Message: "Error uploading file!",
+			})
+			return
+		}
+		util.WriteJson(w, 201, BucketItemResult{
+			Filename: fmt.Sprintf("%s.%s", file, s[len(s)-1]),
+		})
 	})
 
 	return r
